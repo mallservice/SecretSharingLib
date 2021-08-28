380dc865acfd5c2e7bd214507d748201078f2903
久遠恵 <35366719+keihisato@users.noreply.github.com>
2021-08-29 01:45:38 +0900

    first commit

---
+++ Tests/SecretSharingLibTests/SecretSharingLibTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import SecretSharingLib
+
+    final class SecretSharingLibTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+           
+        }
+    }
